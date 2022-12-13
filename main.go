7093343f40a6ea72bf88773817f9ea0e42f6ed25
insane-nomad <daglibdaglib@gmail.com>
2022-12-13 21:41:32 +0300

    GoFiber added

--- main.go
+++ main.go
@@ -1,10 +1,53 @@
-// mynotes project main.go
 package main
 
 import (
-	"fmt"
+	"log"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	// Create a new engine
+	engine := html.New("./ui/html", ".html")
+
+	// Pass the engine to the Views
+	app := fiber.New(fiber.Config{
+		Views:                engine,
+		CompressedFileSuffix: ".compressed.gz",
+	})
+
+	//app.Static("ui/html/static", "ui/html/static")   //рабочая строка
+	app.Static("ui/html/static", "ui/html/static", fiber.Static{
+		Compress:      true,
+		ByteRange:     true,
+		Browse:        true,
+		CacheDuration: 1000 * time.Hour,
+		MaxAge:        3600,
+	})
+
+	app.Get("/", MainPageHandler)
+
+	app.Get("/layout", func(c *fiber.Ctx) error {
+		// Render index within layouts/main
+		return c.Render("index2", fiber.Map{
+			"Title": "Hello, World!",
+		}, "layouts/main")
+	})
+
+	app.Use(func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusNotFound).Render("index", fiber.Map{
+			"Error": "Error 404. Not found!",
+			"Title": "Error 404. Not found!",
+		}, "errors/404")
+	})
+
+	log.Fatal(app.Listen(":3000"))
+}
+func MainPageHandler(c *fiber.Ctx) error {
+	// Render index
+	return c.Render("index", fiber.Map{
+		"Title": "Hello, World!",
+	})
 }
